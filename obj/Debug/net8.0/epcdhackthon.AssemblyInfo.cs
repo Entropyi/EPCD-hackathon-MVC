855369450e403b427dc7cee5c4aaf5aca9c42f71
entropyi <jemo1x503@gmail.com>
2024-10-31 03:31:34 +0300

    font

--- obj/Debug/net8.0/epcdhackthon.AssemblyInfo.cs
+++ obj/Debug/net8.0/epcdhackthon.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("epcdhackthon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68234b3a8fd0fe12eebbb5b16e80bf7e3cfb1c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("epcdhackthon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("epcdhackthon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
