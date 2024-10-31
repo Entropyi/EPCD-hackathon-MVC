d00f6d49c88a0c1a79d26dad54feae7f34a0dde0
entropyi <jemo1x503@gmail.com>
2024-10-31 05:07:28 +0300

    font

--- obj/Release/net8.0/epcdhackthon.AssemblyInfo.cs
+++ obj/Release/net8.0/epcdhackthon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-epcdhackthon-e8ad9400-a57d-4b87-bc8b-e6ef67d71514")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("epcdhackthon")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a849460a883bc8971e4d3a1e3538d708ddb1b15")]
 [assembly: System.Reflection.AssemblyProductAttribute("epcdhackthon")]
